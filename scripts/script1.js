e77961b8b29bc5cf63a08fd76c8bb46c9d33bd60
Ahmed <ahmedmmy97@gmail.com>
2018-10-16 23:54:34 +0300

    CV -> Article added

--- scripts/script1.js
+++ scripts/script1.js
@@ -5,7 +5,9 @@ var projects= "<table><tr><td><strong>2016</strong></td><td>Travel Agency System
 var conf= "<table><tr><td><strong>2018</strong></td><td>Firebase Summit</td></tr><tr><td><strong>2018</strong></td><td>Pacific Northwest Google Summit</td></tr><tr><td><strong>2019</strong></td><td>Chrome Dev Summit</td></tr><tr><td><strong>2020</strong></td><td>Google I/O</td></tr><tr><td><strong>2022</strong></td><td>IBM Interconnect</td></tr></table>";
 var cert= "<table><tr><td><strong>2018</strong></td><td>CCA-Certificate-Java Level 1</td></tr><tr><td><strong>2019</strong></td><td>CCA-Certificate-Java Level 2</td></tr><tr><td><strong>2020</strong></td><td>Android Nanodegree Certificate</td></tr></table>";
 var work= "<table><tr><td>Desktop Application Developer | Freelancer.</td></tr><tr><td>Android Developer |Freelancer</td></tr><tr><td>Android Developer</td></tr></table>"
-var patent="<table><tr><td><strong>Project Aim: </strong></td><td>Home automation system using a simple Android app, which you can use to control electrical appliances with clicks or voice commands. Commands are sent via Bluetooth to Arduino Uno. So you need not get up to switch on or switch off the device while watching a movie or doing some work.</td></tr></table>"
+var patent="<table><tr><td><strong>Project Aim: </strong></td></tr><tr><td>Home automation system using a simple Android app, which you can use to control electrical appliances with clicks or voice commands. Commands are sent via Bluetooth to Arduino Uno. So you need not get up to switch on or switch off the device while watching a movie or doing some work.</td></tr></table>"
+var article ="<table><tr><td></td><td>Kumar, Shiu, and Seong Ro Lee. 'Android based smart home system with control via Bluetooth and internet connectivity.' Consumer Electronics (ISCE 2014), The 18th IEEE International Symposium on. IEEE, 2024.</td></tr><tr><td></td><td><a href='https://ieeexplore.ieee.org/abstract/document/6884302/' target='_blank'>Contents</a></td></tr></table>"
+
 $(document).ready(
     function() {
         $('#p1').html(prog);
@@ -32,7 +34,10 @@ var progFunction =function(){
            
      };
 var patentFunction=function(){
-    document.getElementById("p1").innerHTML=patent
+    document.getElementById("p1").innerHTML=patent;
+}
+var articleFunction=function(){
+    document.getElementById("p1").innerHTML=article;
 }
   $(function(){
     $('.additional-menu > li').click(function(){
